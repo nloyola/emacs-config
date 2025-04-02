740cf19667b785579bcb06dd9918f49696e75b3a
Nelson Loyola <nloyola@gmail.com>
2025-04-02 07:34:05 +0200

    Function to open an alacritty window in current folder

---
+++ users/asterix/open-alacritty.el
@@ -0,0 +1,9 @@
+(defun nl/open-alacritty-here ()
+  "Open a new Alacritty terminal window in the current buffer's directory."
+  (interactive)
+  (let* ((dir (cond
+               ((derived-mode-p 'dired-mode) (dired-current-directory))
+               ((buffer-file-name) (file-name-directory (buffer-file-name)))
+               (t default-directory))))
+    (when dir
+      (start-process "alacritty" nil "alacritty" "--working-directory" dir))))
