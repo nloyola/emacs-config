3dd9dd26c0fac897cd5cec9d8141fc422aef4929
Nelson Loyola <nloyola@gmail.com>
2026-06-22 08:23:22 +0000

    Fix for running on terminal

--- init.el
+++ init.el
@@ -61,10 +61,10 @@
 (setq comp-deferred-compilation t)
 
 ;; Turn off mouse interface early in startup to avoid momentary display
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(tooltip-mode -1)
-(fringe-mode -1)
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tooltip-mode) (tooltip-mode -1))
+(if (fboundp 'fringe-mode) (fringe-mode -1))
 
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
