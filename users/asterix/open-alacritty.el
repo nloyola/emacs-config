0025a2144e4a980775629b3e338a3f905129d003
Nelson Loyola <nloyola@gmail.com>
2026-07-10 12:46:41 +0200

    fix(emacs): launch alacritty outside emacs env

--- users/asterix/open-alacritty.el
+++ users/asterix/open-alacritty.el
@@ -1,6 +1,19 @@
 ;;; -*- lexical-binding: t; -*-
 ;;;
 
+(defun nl/open-alacritty--detached (dir)
+  "Open a detached Alacritty terminal window in DIR."
+  (let ((process-environment (copy-sequence process-environment)))
+    ;; Do not let the spawned shell think it is running inside Emacs.  The zsh
+    ;; config intentionally uses a minimal prompt when INSIDE_EMACS is set.
+    (setenv "INSIDE_EMACS" nil)
+    (setenv "EMACS" nil)
+    (call-process
+     "setsid" ;; fully detaches from Emacs process group
+     nil 0 nil
+     "env" "-u" "INSIDE_EMACS" "-u" "EMACS"
+     "alacritty" "--working-directory" dir)))
+
 (defun nl/open-alacritty-here ()
   "Open a new Alacritty terminal window in the current buffer's directory, detached from Emacs."
   (interactive)
@@ -9,10 +22,7 @@
                ((buffer-file-name) (file-name-directory (buffer-file-name)))
                (t default-directory))))
     (when dir
-      (call-process
-       "setsid" ;; fully detaches from Emacs process group
-       nil 0 nil
-       "alacritty" "--working-directory" dir))))
+      (nl/open-alacritty--detached dir))))
 
 (defun nl/open-alacritty-project-root ()
   "Open a new Alacritty terminal window in the current project root, detached from Emacs."
@@ -20,7 +30,4 @@
   (require 'projectile)
   (let ((dir (projectile-project-root)))
     (when dir
-      (call-process
-       "setsid" ;; fully detaches from Emacs process group
-       nil 0 nil
-       "alacritty" "--working-directory" dir))))
+      (nl/open-alacritty--detached dir))))
